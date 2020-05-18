509b9d1f680329ba37a043d23388e99d5195122f
Euan Kemp <euank@euank.com>
2020-05-17 19:01:41 -0700

    inspircd: init at v3.6.0

--- default.nix
+++ default.nix
@@ -2,4 +2,6 @@ self: super: rec {
   haskellPackages = (import ./pkgs/gitit/default.nix) self super;
   # Add a top-level alias, might as well
   gitit = haskellPackages.gitit;
+
+  inspircd = super.callPackage ./pkgs/applications/networking/irc/inspircd {};
 }
---
+++ pkgs/applications/networking/irc/inspircd/default.nix
@@ -0,0 +1,61 @@
+{ stdenv, fetchFromGitHub, pkgconfig, openssl, gnutls, perl }:
+
+stdenv.mkDerivation rec {
+  pname = "inspircd";
+  version = "3.6.0";
+
+  # A reasonable set of extra modules to build
+  extras = [ "m_ssl_openssl.cpp" "m_ssl_gnutls.cpp" "m_sslrehashsignal.cpp" "m_regex_posix.cpp" ];
+
+  src = fetchFromGitHub {
+    owner = "inspircd";
+    repo = "inspircd";
+    sha256 = "1g78qahxaz75c4158p2fid7rzd5dxbgm80gg3fzswsk1pwl1vh9p";
+    rev = "v${version}";
+  };
+
+  nativeBuildInputs = [ perl openssl gnutls pkgconfig ];
+
+  # So, this package is still not ready for prime-time. Why not?
+  # Well, two reasons:
+  # 1. the postInstallPhase include stuff below doesn't actually work. No clue
+  # why.
+  # 2. include probably is being handled wrong, as are modules
+  # So for 2, what do I mean? Ideally we want the longer-term thing to be that
+  # the inspircd bin is one output (what you run), the modules are another
+  # (which the bin loads dynamically), and the include is a third (which is
+  # used for compiling out-of-tree modules).
+  # Using multiple outputs is something I'm not super familiar with, so I
+  # didn't get it working to my satisfaction.
+  # I'm going to just leave this bad derivation off in my tree and use it for
+  # now, but I'll go back and ask for help on #nixos or on a PR in the future
+  # and get this merged.
+  configurePhase = ''
+    patchShebangs ./configure ./make/unit-cc.pl
+
+    ./configure --enable-extras=${builtins.concatStringsSep "," extras}
+    ./configure --disable-interactive \
+      --disable-auto-extras \
+      --prefix=$prefix \
+      --manual-dir=$outputDoc \
+      --binary-dir=$outputBin
+  '';
+
+  postInstallPhase = ''
+    mkdir -p $out/include
+    cp -R $src/include $out/include
+  '';
+
+  fixupPhase = ''
+    mkdir -p $out/bin
+    mv $out/inspircd $out/bin/inspircd
+  '';
+
+  meta = {
+    homepage    = "https://www.inspircd.org/";
+    description = "A modular C++ IRC server";
+    platforms   = stdenv.lib.platforms.unix;
+    maintainers = with stdenv.lib.maintainers; [ euank ];
+    license     = stdenv.lib.licenses.gpl2Plus;
+  };
+}
