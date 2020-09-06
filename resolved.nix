717220d88852e98f45233e2f8dd4ab47396a96ef
Euan Kemp <euank@euank.com>
2020-09-05 20:11:05 -0700

    resolved: optionally allow passing in a config

--- resolved.nix
+++ resolved.nix
@@ -1,9 +1,11 @@
+{ config ? {}}:
 # This contains a resolved version of this overlay, useful for building images within it, testing, etc.
 let
   nixpkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/0f976477e9f61578c5f7f0a33830cfaa81168d75.tar.gz") {
     overlays = [
       (import ./.)
     ];
+    inherit config;
   };
 in
   nixpkgs
