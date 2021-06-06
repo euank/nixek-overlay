d1e2adc0b191b194fbbf382a48f1c9d238e63209
Euan Kemp <euank@euank.com>
2021-06-05 20:40:09 -0700

    maptool: 1.7.0 -> 1.8.5

--- flake.nix
+++ flake.nix
@@ -1,12 +1,25 @@
 {
   description = "my overlay";
 
-  outputs = { self, nixpkgs }: rec {
-    overlay = import ./default.nix;
-    packages.x86_64-linux = (import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [ self.overlay ];
-      config = { allowUnfree = true; };
-    });
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-20_09.url = "github:NixOs/nixpkgs/5622b6b6feb669edc227aaf000413d5b593d4051";
   };
+
+  outputs = { self, nixpkgs, nixpkgs-20_09 }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          (final: prev: {
+            openjdk14 = nixpkgs-20_09.outputs.legacyPackages.x86_64-linux.pkgs.openjdk14;
+          })
+          self.overlay
+        ];
+        config = { allowUnfree = true; };
+      };
+    in {
+      overlay = import ./default.nix;
+      packages.x86_64-linux = pkgs;
+    };
 }
--- pkgs/games/maptool/default.nix
+++ pkgs/games/maptool/default.nix
@@ -1,35 +1,25 @@
-{ callPackage, gradleGen, makeWrapper, fetchgit, lib, pkgs, ... }:
+{ callPackage, gradleGen, openjdk14, makeWrapper, fetchgit, lib, pkgs, ... }:
+
 let
-  buildGradle = callPackage ./gradle-env.nix {
-    gradleGen = gradleGen.override {
-      java = pkgs.jdk11;
-    };
-  };
+  buildGradle = callPackage ./gradle-env.nix { };
 in
-buildGradle {
-  version = "1.7.0";
+buildGradle rec {
+  version = "1.8.5";
+  rev = "8ed806d1204d8e49d7f87268e06f5c8dd83af8ef";
   envSpec = ./gradle-env.json;
+  buildJdk = openjdk14;
 
-  # Patched version from my github so this all works
   src = fetchgit {
-    url = "https://github.com/euank/maptool.git";
-    rev = "697e9549cf42ac84fe03d74b30ae2f761868f974";
-    sha256 = "1ynkklvgr8sb9nspvij8mg4ndn0ss7739blj0l7limb6y2hjabp0";
-    # Used for versioning, TODO mock this out somehow
-    leaveDotGit = true;
+    inherit rev;
+    url = "https://github.com/euank/maptool";
+    sha256 = "sha256-68kF/ujflLvjYJFTiCX9aqrCL4ynfwOwYrQxdcxZ0iA=";
   };
 
-  gradleFlags = [ "distTar" ];
 
-  buildInputs = [ pkgs.makeWrapper ];
+  gradleFlags = [ "-PnoGit=true" "-PgitTag=${version}" "-PgitCommit=${rev}" "distTar" ];
 
   installPhase = ''
-    mkdir -p $out/realroot $out/bin
-    tar -C "$out/realroot" --strip-components=1 -xf build/distributions/MapTool.tar
-
-    patchShebangs $out/realroot/bin/MapTool
-
-    makeWrapper $out/realroot/bin/MapTool $out/bin/MapTool --prefix PATH : ${lib.makeBinPath [ pkgs.jdk11 ]}
-
+    mkdir -p $out
+    tar -xvf build/distributions/MapTool.tar --strip-components=1 -C $out
   '';
 }
